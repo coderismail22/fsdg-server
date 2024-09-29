5beacf53154a0ceae470dc77facc3e217c61c955
ismail <coderismail22@gmail.com>
2024-09-29 22:35:36 +0600

    update deploy

--- src/api/index.js
+++ src/api/index.js
@@ -50,6 +50,11 @@ mongoose
   .then(() => console.log("MongoDB connected!"))
   .catch((err) => console.error(err));
 
+  app.use("/",()=>{
+    "Server is running"
+  })
+
+
 app.use("/api/posts", postRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/sendmail", emailRoutes);
@@ -58,8 +63,8 @@ app.use((req, res) => {
   res.status(404).send({ message: "API Not Found" });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running on port ${process.env.PORT}`);
+// });
 
 export default app;
